7fe21bd2f3d164b845d8ced89edf962c5fb502d6
Nrgn <mejumine@gmail.com>
2022-11-27 10:56:09 +0100

    tout les trucs

--- ft_tolower.c
+++ ft_tolower.c
@@ -1,34 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*   ft_tolower.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: romaurel <rxonrgn@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/27 01:03:10 by romaurel          #+#    #+#             */
-/*   Updated: 2022/11/27 01:03:10 by romaurel         ###   ########.fr       */
+/*   Created: 2022/11/27 10:05:43 by romaurel          #+#    #+#             */
+/*   Updated: 2022/11/27 10:05:43 by romaurel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dst, const char *src, size_t size)
+int	ft_tolower(int c)
 {
-	size_t	i;
-	size_t	j;
-	size_t	tdest;
-	size_t	tsrc;
-
-	tsrc = ft_strlen(src);
-	if (size < 1)
-		return (tsrc + size);
-	tdest = ft_strlen(dst);
-	i = tdest;
-	j = 0;
-	while (src[j] && i < size - 1)
-		dst[i++] = src[j++];
-	dst[i] = 0;
-	if (size < tdest)
-		return (tsrc + size);
-	return (tdest + tsrc);
+	if (c >= 'A' && c <= 'Z')
+		return (c += 32);
+	return (c);
 }
