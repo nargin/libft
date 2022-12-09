c4b4acfe7471b11e18b713893d9227835c53f9f5
Nargin <mejumine@gmail.com>
2022-12-09 11:33:57 -0500

    calloc en mode bon la

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: romaurel <rxonrgn@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/03 13:27:27 by romaurel          #+#    #+#             */
-/*   Updated: 2022/12/03 13:39:46 by romaurel         ###   ########.fr       */
+/*   Updated: 2022/12/09 11:32:26 by romaurel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,10 @@ void	*ft_calloc(size_t n, size_t size)
 {
 	void	*array;
 
-	if (!n)
-		n = 1;
-	if (!size)
-		size = 1;
+	if (!n || !size)
+		ft_calloc(1, 1);
+	if (n * size / n != size)
+		return (0);
 	array = malloc(n * size);
 	if (array)
 		ft_bzero(array, n * size);
