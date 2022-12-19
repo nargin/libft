b8c04654b2f73cb82a59fc2cf8fceb55631c4c99
Nrgn <mejumine@gmail.com>
2022-12-19 16:25:57 +0100

    lstsize lstlast all ok tester

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: romaurel <rxonrgn@gmail.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/27 00:39:46 by romaurel          #+#    #+#             */
-/*   Updated: 2022/12/18 01:49:26 by romaurel         ###   ########.fr       */
+/*   Updated: 2022/12/19 16:22:11 by romaurel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -94,5 +94,9 @@ char	**ft_split(char const *s, char c);
 t_list	*ft_lstnew(void *content);
 
 void	ft_lstadd_front(t_list **lst, t_list *new);
+
+int		ft_lstsize(t_list *lst);
+
+t_list	*ft_lstlast(t_list *lst);
 
 #endif
