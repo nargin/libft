10c0733ba974e399854fb7477c62df72c017f7da
Nrgn <mejumine@gmail.com>
2023-01-07 20:32:55 +0100

    lstadd back test

---
+++ ft_lstadd_back.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: romaurel <rxonrgn@gmail.com>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/07 17:32:29 by romaurel          #+#    #+#             */
+/*   Updated: 2023/01/07 17:32:29 by romaurel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	if (!new)
+		return ;
+	if (!*lst)
+	{
+		*lst = new;
+		return ;
+	}
+	while (*lst->next)
+		*lst = *lst->next;
+	*lst->next = new;
+}
